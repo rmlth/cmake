0c126e47f22f88f509833f392db7ca409f354d5d
lth123456 <2284268273@qq.com>
2024-05-25 15:45:15 +0800

    init🎉:

---
+++ t2/build/bin/CMakeFiles/hell0.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hell0.dir/main.cpp.o"
+  "CMakeFiles/hell0.dir/main.cpp.o.d"
+  "hell0"
+  "hell0.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hell0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ t2/build/bin/CMakeFiles/hell0.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for hell0.
+# This may be replaced when dependencies are built.
