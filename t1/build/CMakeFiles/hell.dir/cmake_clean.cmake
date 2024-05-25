f9661e25219adcbb70e882e70b28bca66eae2c1f
lth123456 <2284268273@qq.com>
2024-05-25 14:55:23 +0800

    docs📝: cmake test

---
+++ t1/build/CMakeFiles/hell.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hell.dir/main.cpp.o"
+  "CMakeFiles/hell.dir/main.cpp.o.d"
+  "hell"
+  "hell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
